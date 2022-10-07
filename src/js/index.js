17a7636f36a3875f07ab38b651ad2784a9d563d5
Muhammad Shaheer <shaheer@uptek.com>
2022-10-07 10:01:30 +0500

    Animation added

--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,48 @@
+
+
 const clients = document.querySelectorAll('.clients');
+const reviewContent = document.querySelectorAll('.review-content');
 
 for (let i = 0; i < clients.length; i++) {
-  clients[i].addEventListener('click', (e)=> {
-    clients.forEach((item) => {
+  clients[i]?.addEventListener('click', (e)=> {
+    clients.forEach((item, i) => {
       item.classList.remove('active')
+      reviewContent[i].classList.add('hidden')
     })
-    clients[i].classList.add('active')
+    clients[i]?.classList.add('active')
+    let selectedReview = clients[i].getAttribute('data-for');
+    let reviewId = reviewContent[i].getAttribute('id');
+
+    if(selectedReview == reviewId) {
+      reviewContent[i]?.classList.remove('hidden')
+    }
   })
 }
 
-console.log(clients)
+
+var a = 0;
+$(window).scroll(function () {
+  var oTop = $('#counter').offset().top - window.innerHeight;
+  if (a == 0 && $(window).scrollTop() > oTop) {
+    $('.counter-value').each(function () {
+      var $this = $(this),
+        countTo = $this.attr('data-count');
+      $({
+        countNum: $this.text()
+      }).animate({
+        countNum: countTo
+      },
+        {
+        duration: 2000,
+        easing: 'swing',
+        step: function () {
+          $this.text(Math.floor(this.countNum));
+        },
+        complete: function () {
+          $this.text(this.countNum);
+        }
+        });
+    });
+    a = 1;
+  }
+});
